71476f1d60683800db1750ae485451a5773ee7da
Jordan Olshevski <jordanolshevski@gmail.com>
2015-08-25 22:07:21 -0500

    Remove assertion logic
    This commit removes the type & provider's assertion logic. The new
    direction will involve a Puppet application which compiles the catalog
    containing the testcases in the form of assertion declarations.
    Assertions will then be made against the catalog.

--- lib/puppet/provider/assertion/resource.rb
+++ lib/puppet/provider/assertion/resource.rb
@@ -1,21 +1,7 @@
 Puppet::Type.type(:assertion).provide(:resource) do
-
-  # Assert takes the name of a resource parameter and
-  # asserts on its equality to the corresponding
-  # field on the assertion resource.
-  #
-  # If the subject's type does not support the given
-  # parameter, it will raise an appropriate error.
-  # Similarly, if the assertion fails, an error will
-  # be raised.
-  def assert(param)
-    unless @resource[:subject].class.valid_parameter?(param)
-      raise Puppet::Error, "Cannot make an assertion on the value of parameter '#{param}' because the resource #{@resource[:subject].to_s} does not support it."
-    end
-
-    unless @resource[:subject][param] == @resource[param]
-      raise Puppet::Error, "Assertion failed on parameter #{param}. Wanted value '#{@resource[param]}' got '#{@resource[:subject][param]}'"
-    end
-  end
-
+  # The resource assertion provider is just a stub.
+  # Assertion logic is handled from the spec application,
+  # the type/provider are simply a means of getting
+  # assertion data into the catalog where it will
+  # be evaluated by the application.
 end
--- lib/puppet/type/assertion.rb
+++ lib/puppet/type/assertion.rb
@@ -1,20 +1,14 @@
-class Puppet::Parameter::Assertable < Puppet::Parameter
-  # Retrieve simply invokes the provider and passes
-  # the params's name
-  def retrieve
-    @resource.provider.assert(name)
-  end
-end
+class Puppet::Parameter::Assertable < Puppet::Parameter; end
 
 Puppet::Type.newtype(:assertion) do
 
   @doc = "Makes assertions on the state of a resource in the catalog.
 
-  The assertion type is intended to serve as a means of natively testing Puppet code.
+  The assertion type defines an assertion that will be evaluated by the
+  spec application.
+
   Arbirary parameters are allowed. Each will result in an equality assertion against
   the corresponding attribute of the subject resource (see the subject parameter).
-
-  If any of the assertions fail, an error will be logged.
   "
 
   newparam(:name) do
@@ -25,27 +19,10 @@ Puppet::Type.newtype(:assertion) do
   end
 
   newparam(:subject) do
-    # Retrieve takes the resource reference and
-    # resolves it to the instance of the referenced
-    # resource found in the catalog.
-    def retrieve
-      @resource[:subject] = @resource.catalog.resource(@resource[:subject].to_s)
-    end
-
     desc "A reference to the resource to be asserted upon.
 
     The referenced resource will be the subject of any assertions made as a result of this resource declaration.
     "
-  end
-
-  # Retrieve recurses into each param, checks if
-  # it responds to .retrieve, and if true, calls it.
-  # If the param does not respond to .retrieve, it
-  # will not be called, and no error will be raised.
-  def retrieve
-    parameters.each do |name, param|
-      param.retrieve if param.respond_to?(:retrieve)
-    end
   end
 
   # We reimplement this method in order to allow
