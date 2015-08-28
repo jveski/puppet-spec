36f965e815ffcfe8b52ee53a178fae878e8b1613
Jordan Olshevski <jordanolshevski@gmail.com>
2015-08-28 14:46:55 -0700

    Removing the provider
    This commit was intended to add behavior that would block users from
    accidentally or otherwise declaring the assertion resource in manifests
    that are not compiled under the spec application. Instead, I just
    removed the type to intentionally break compilation. Why not get rid of
    code instead of writing more?

--- lib/puppet/provider/assertion/resource.rb
+++
@@ -1,7 +0,0 @@
-Puppet::Type.type(:assertion).provide(:resource) do
-  # The resource assertion provider is just a stub.
-  # Assertion logic is handled from the spec application,
-  # the type/provider are simply a means of getting
-  # assertion data into the catalog where it will
-  # be evaluated by the application.
-end
