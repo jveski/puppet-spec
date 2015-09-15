991367e10ee5d980aaf0d6da244361398d563dbc
Jordan Olshevski <jordanolshevski@gmail.com>
2015-09-15 11:55:15 -0700

    Revert "Removing subject type validation"
    This reverts commit 134f4ddb99380ca8bb17ddd9cfeaddd5da4ade00.

--- spec/unit/puppet/type/assertion_spec.rb
+++ spec/unit/puppet/type/assertion_spec.rb
@@ -68,6 +68,41 @@ describe Puppet::Type.type(:assertion) do
   end
 end
 
+describe Puppet::Type::Assertion::ParameterSubject do
+  let(:the_resource) { stub }
+  subject { Puppet::Type::Assertion::ParameterSubject.new(:resource => the_resource) }
+
+  describe ".validate" do
+    context "when given a string" do
+      it "should raise an error" do
+        expect{subject.validate('test')}.to raise_error(
+          'Subject must be a resource reference'
+        )
+      end
+    end
+
+    context "when given a hash" do
+      it "should raise an error" do
+        expect{subject.validate({:key => :value})}.to raise_error(
+          'Subject must be a resource reference'
+        )
+      end
+    end
+
+    context "when given a Puppet resource" do
+      let(:the_resource) do
+        Puppet::Type.type(:package).new(
+          :name => 'the resource',
+        ).to_resource
+      end
+
+      it "should should not raise an error" do
+        expect{subject.validate(the_resource)}.to_not raise_error
+      end
+    end
+  end
+end
+
 describe Puppet::Type::Assertion::ParameterEnsure do
   let(:the_resource) { stub }
   subject { Puppet::Type::Assertion::ParameterEnsure.new(:resource => the_resource) }
